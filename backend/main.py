6132e54221c70917b5bc22f140cdad9e294ade0b
david <davideli914@gmail.com>
2026-03-22 23:19:01 +0200

    removing all trading missions

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,20 @@
 import os
 import serverless_wsgi  # You'll need to add this to requirements.txt
-from scheduler.daily_task import daily_task
-from scheduler.scheduler import start_scheduler
+# from scheduler.daily_task import daily_task
+# from scheduler.scheduler import start_scheduler
 from server.app import app
 
 def handler(event, context):
     # If the "source" is EventBridge, run the stock fetching logic
-    if event.get('source') == 'aws.events':
-        print("Alarm went off! Fetching stock data...")
-        daily_task()
-        return {"status": "success"}
+    # if event.get('source') == 'aws.events':
+    #     print("Alarm went off! Fetching stock data...")
+    #     daily_task()
+    #     return {"status": "success"}
     
     # Otherwise, treat it like a normal web request for your Flask app
     return serverless_wsgi.handle_request(app, event, context)
 
 # Keep this for local testing only
 if __name__ == '__main__':
-    start_scheduler()
+    # start_scheduler()
     app.run(debug=True, host='0.0.0.0', port=8080)
