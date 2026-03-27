666b9bc5085b72776bf57c3389f06786c1db510c
david <davideli914@gmail.com>
2026-03-27 18:20:23 +0300

    make mongo connection a standalone function so lambda can execute it after env vars import

--- backend/mongo_utils.py
+++ backend/mongo_utils.py
@@ -2,78 +2,104 @@ import os
 from pymongo import MongoClient
 from Enums import rl_variables
 
-MONGO_URI = os.getenv("MONGO_URI",
-                      "mongodb+srv://adm:Aa123456@karli.mongocluster.cosmos.azure.com/?tls=true&authMechanism=SCRAM-SHA-256&retrywrites=false&maxIdleTimeMS=120000")
-client = MongoClient(MONGO_URI)
-db = client["KaRLi"]
-users_collection = db["users"]
-stocksDB = client["stock_data_db"]
-norm_collection = stocksDB["daily_stock_data_normalized"]
-meta_collection = stocksDB["normalization_metadata"]
+def create_client():
+    MONGO_URI = os.getenv("MONGO_URI")
+    client = MongoClient(MONGO_URI)
+    db = client["KaRLi"]
+    users_collection = db["users"]
+    stocksDB = client["stock_data_db"]
+    norm_collection = stocksDB["daily_stock_data_normalized"]
+    meta_collection = stocksDB["normalization_metadata"]
 
+    return client, users_collection, stocksDB, norm_collection, meta_collection
 
 def sign_up(username, password, age, broker_api_key, broker_api_secret):
-    document = users_collection.find_one({"username": username})
+    client, users_collection, _, _, _ = create_client()
+    try:
+        document = users_collection.find_one({"username": username})
 
-    if document:
-        return False
-    users_collection.insert_one({
-        "username": username,
-        "password": password,
-        "age": age,
-        "brokerApiKey": broker_api_key,
-        "brokerApiSecret": broker_api_secret
-    })
-    return True
+        if document:
+            return False
+        users_collection.insert_one({
+            "username": username,
+            "password": password,
+            "age": age,
+            "brokerApiKey": broker_api_key,
+            "brokerApiSecret": broker_api_secret
+        })
+        return True
+    finally:
+        client.close()
 
 
 def sign_in(username, password):
-    document = users_collection.find_one({"username": username, "password": password})
-
-    if document:
-        return True
-    return False
+    client, users_collection, _, _, _ = create_client()
+    try:
+        document = users_collection.find_one({"username": username, "password": password})
+        return bool(document)
+    finally:
+        client.close()
 
 
 def get_user_brokerApi_credentials(username):
-    document = users_collection.find_one({"username": username})
+    client, users_collection, _, _, _ = create_client()
+    try:
+        document = users_collection.find_one({"username": username})
 
-    if document:
-        return {
-            "api_key": document.get("brokerApiKey"),
-            "api_secret": document.get("brokerApiSecret")
-        }
-    return None
+        if document:
+            return {
+                "api_key": document.get("brokerApiKey"),
+                "api_secret": document.get("brokerApiSecret")
+            }
+        return None
+    finally:
+        client.close()
 
 def get_all_users_with_credentials():
-    users = []
+    client, users_collection, _, _, _ = create_client()
+    try:
+        users = []
 
-    for user in users_collection.find({}):
-        username = user.get("username")
-        api_key = user.get("brokerApiKey")
-        api_secret = user.get("brokerApiSecret")
+        for user in users_collection.find({}):
+            username = user.get("username")
+            api_key = user.get("brokerApiKey")
+            api_secret = user.get("brokerApiSecret")
 
-        users.append({
-            "username": username,
-            "api_key": api_key,
-            "api_secret": api_secret,
-        })
+            users.append({
+                "username": username,
+                "api_key": api_key,
+                "api_secret": api_secret,
+            })
 
-    return users
+        return users
+    finally:
+        client.close()
 
 def load_stats(ticker: str) -> dict:
-    stats = {d["feature"]: (d["mean"], d["std"])
-             for d in meta_collection.find({"ticker": ticker})}
-    missing = set(rl_variables.FEATURE_COLS) - set(stats)
-    if missing:
-        raise ValueError(f"metadata missing for {missing}")
-    return stats
+    client, _, _, _, meta_collection = create_client()
+    try:
+        stats = {d["feature"]: (d["mean"], d["std"])
+                 for d in meta_collection.find({"ticker": ticker})}
+        missing = set(rl_variables.FEATURE_COLS) - set(stats)
+        if missing:
+            raise ValueError(f"metadata missing for {missing}")
+        return stats
+    finally:
+        client.close()
 
 def insert_daily_data(tickers_data):
-    records = tickers_data.to_dict(orient='records')
+    client, _, _, norm_collection, _ = create_client()
+    try:
+        records = tickers_data.to_dict(orient='records')
 
-    if records:
-        norm_collection.insert_many(records)
+        if records:
+            norm_collection.insert_many(records)
+    finally:
+        client.close()
 
 def fetch_data_for_inference(ticker: str, window_size: int = 30):
-    return norm_collection.find({"ticker": ticker}).sort("date", -1).limit(window_size)
+    client, _, _, norm_collection, _ = create_client()
+    try:
+        return norm_collection.find({"ticker": ticker}).sort("date", -1).limit(window_size)
+    finally:
+        client.close()
